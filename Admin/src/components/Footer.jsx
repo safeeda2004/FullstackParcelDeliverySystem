c542b45e1a79113583f085ff76e192ff5f3747b2
mariyam safeeda <mariyam.safeeda.mardala@gmail.com>
2024-11-21 13:03:01 +0530

    started working on admin side

---
+++ Admin/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+const  Footer = () => {
+  return (
+    <div className="text-2xl text-white">Footer</div>
+     
+  )
+}
+
+export default Footer
---
+++ Admin/src/pages/Parcels.jsx
@@ -0,0 +1,9 @@
+
+const Parcels = () => {
+  return (
+    <div className="text-2xl text-white">Parcels</div>
+         
+  )
+}
+
+export default Parcels
